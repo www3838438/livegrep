64f171394f806dfe81c66419a3b32342728917df
Brandon Rhodes <brhodes@dropbox.com>
2018-01-11 16:23:38 -0500

    Get both green and red highlights working

--- web/htdocs/assets/raw-js/blame.js
+++ web/htdocs/assets/raw-js/blame.js
@@ -1,10 +1,13 @@
 (function f() {
-    $('body').on('mouseenter', '.hashes > a', function(e) {
-        commitHash = $(e.target).attr('href');
-        $('.hashes a[href="' + commitHash + '"]').addClass('bump');
+    var body = $('body');
+    body.on('mouseenter', '#hashes > a', function(e) {
+        var commitHash = $(e.target).attr('href');
+        var cls = 'highlight ' + commitHash.substr(commitHash.length - 1);
+        $('#hashes a[href="' + commitHash + '"]').addClass(cls);
     });
-    $('body').on('mouseleave', '.hashes > a', function(e) {
-        commitHash = $(e.target).attr('href');
-        $('.hashes a[href="' + commitHash + '"]').removeClass('bump');
+    body.on('mouseleave', '#hashes > a', function(e) {
+        var commitHash = $(e.target).attr('href');
+        var cls = 'highlight ' + commitHash.substr(commitHash.length - 1);
+        $('#hashes a[href="' + commitHash + '"]').removeClass(cls);
     });
 })();
