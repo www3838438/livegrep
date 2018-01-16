04b22be88b2062d1e4caf08ad3a363107fab409b
Brandon Rhodes <brhodes@dropbox.com>
2018-01-16 16:58:19 -0500

    Rework blame logic as new FileCommit step() method

--- blameworthy/indexer.go
+++ blameworthy/indexer.go
@@ -1,8 +1,8 @@
 package blameworthy
 
 import (
-	"fmt"
-	"strings"
+//	"fmt"
+//	"strings"
 )
 
 type BlameSegment struct {
@@ -12,146 +12,108 @@ type BlameSegment struct {
 
 type BlameSegments []BlameSegment;
 
-type HalfEntry struct {		// For a specific path and commit
-	PreviousCommit string
-	segments BlameSegments
-}
-
-type HalfIndex map[string]HalfEntry;
-
-type BlameEntry struct {	// For a specific path and commit
-	PreviousCommit string
-	NextCommit string
-	Blame BlameSegments
-	Future BlameSegments
-}
-
-type BlameIndex map[string]BlameEntry;
-
 type BlameVector []string	// Blames every line on a commit hash
 
-type BlameResult struct {
-	PreviousCommit string
-	NextCommit string
-	Blame BlameVector
-	Future BlameVector
-}
+// func Build_index(commits CommitHistory) (*BlameIndex) {
+// 	forward_index, fileLengths := Build_half_index(commits, nil)
+// 	reverse_history_in_place(commits)
+// 	reverse_index, _ := Build_half_index(commits, fileLengths)
 
-func Build_index(commits CommitHistory) (*BlameIndex) {
-	forward_index, file_lengths := Build_half_index(commits, nil)
-	reverse_history_in_place(commits)
-	reverse_index, _ := Build_half_index(commits, file_lengths)
+// 	// TODO: avoid having to put everything back by maybe switching
+// 	// to an iterator or something?
+// 	reverse_history_in_place(commits)
 
-	// TODO: avoid having to put everything back by maybe switching
-	// to an iterator or something?
-	reverse_history_in_place(commits)
+// 	blame_index := make(BlameIndex)
 
-	blame_index := make(BlameIndex)
+// 	for key, forward_entry := range *forward_index {
+// 		var reverse_segments BlameSegments
 
-	for key, forward_entry := range *forward_index {
-		var reverse_segments BlameSegments
+// 		i := strings.Index(key, ":")
+// 		colon_path := key[i:]  // the colon followed by the path
+// 		mid_entry := (*reverse_index)[key]
+// 		commit_hash := mid_entry.PreviousCommit
 
-		i := strings.Index(key, ":")
-		colon_path := key[i:]  // the colon followed by the path
-		mid_entry := (*reverse_index)[key]
-		commit_hash := mid_entry.PreviousCommit
+// 		if len(forward_entry.segments) == 0 {
+// 			reverse_segments = BlameSegments{}
+// 		} else {
+// 			key2 := commit_hash + colon_path
+// 			reverse_entry, ok := (*reverse_index)[key2]
+// 			if ok {
+// 				reverse_segments = reverse_entry.segments
+// 			} else {
+// 				length := sum_lines(forward_entry.segments)
+// 				reverse_segments = BlameSegments{{length, ""}}
+// 			}
+// 		}
+// 		blame_index[key] = BlameEntry{
+// 			forward_entry.PreviousCommit,
+// 			commit_hash,
+// 			forward_entry.segments,
+// 			reverse_segments,
+// 		}
+// 	}
+// 	return &blame_index
+// }
 
-		if len(forward_entry.segments) == 0 {
-			reverse_segments = BlameSegments{}
-		} else {
-			key2 := commit_hash + colon_path
-			reverse_entry, ok := (*reverse_index)[key2]
-			if ok {
-				reverse_segments = reverse_entry.segments
-			} else {
-				length := sum_lines(forward_entry.segments)
-				reverse_segments = BlameSegments{{length, ""}}
-			}
-		}
-		blame_index[key] = BlameEntry{
-			forward_entry.PreviousCommit,
-			commit_hash,
-			forward_entry.segments,
-			reverse_segments,
-		}
-	}
-	return &blame_index
-}
+// func (ix BlameIndex) GetNode(commit_hash string, path string,
+// ) (*BlameResult, bool) {
+// 	if len(commit_hash) > hashLength {
+// 		commit_hash = commit_hash[:hashLength]
+// 	}
+// 	key := commit_hash + ":" + path
+// 	entry, ok := ix[key]
+// 	if !ok {
+// 		return nil, false
+// 	}
+// 	result := BlameResult{
+// 		entry.PreviousCommit,
+// 		entry.NextCommit,
+// 		flatten_segments(entry.Blame),
+// 		flatten_segments(entry.Future),
+// 	}
+// 	return &result, true
+// }
 
-func (ix BlameIndex) GetNode(commit_hash string, path string,
-) (*BlameResult, bool) {
-	if len(commit_hash) > hashLength {
-		commit_hash = commit_hash[:hashLength]
-	}
-	key := commit_hash + ":" + path
-	entry, ok := ix[key]
-	if !ok {
-		return nil, false
-	}
-	result := BlameResult{
-		entry.PreviousCommit,
-		entry.NextCommit,
-		flatten_segments(entry.Blame),
-		flatten_segments(entry.Future),
-	}
-	return &result, true
-}
+// func (ix BlameIndex) GetFile(commit_hash string, path string,
+// ) (*BlameResult, bool) {
+// 	if len(commit_hash) > hashLength {
+// 		commit_hash = commit_hash[:hashLength]
+// 	}
+// 	key := commit_hash + ":" + path
+// 	entry, ok := ix[key]
+// 	fmt.Print(key, ok, "\n")
+// 	if !ok {
+// 		return nil, false
+// 	}
+// 	if len(entry.Blame) == 0 && commit_hash != entry.PreviousCommit {
+// 		key := entry.PreviousCommit + ":" + path
+// 		entry, ok = ix[key]
+// 		if !ok {
+// 			return nil, false
+// 		}
+// 	}
+// 	result := BlameResult{
+// 		entry.PreviousCommit,
+// 		entry.NextCommit,
+// 		flatten_segments(entry.Blame),
+// 		flatten_segments(entry.Future),
+// 	}
+// 	return &result, true
+// }
 
-func (ix BlameIndex) GetFile(commit_hash string, path string,
-) (*BlameResult, bool) {
-	if len(commit_hash) > hashLength {
-		commit_hash = commit_hash[:hashLength]
-	}
-	key := commit_hash + ":" + path
-	entry, ok := ix[key]
-	fmt.Print(key, ok, "\n")
-	if !ok {
-		return nil, false
-	}
-	if len(entry.Blame) == 0 && commit_hash != entry.PreviousCommit {
-		key := entry.PreviousCommit + ":" + path
-		entry, ok = ix[key]
-		if !ok {
-			return nil, false
-		}
-	}
-	result := BlameResult{
-		entry.PreviousCommit,
-		entry.NextCommit,
-		flatten_segments(entry.Blame),
-		flatten_segments(entry.Future),
-	}
-	return &result, true
-}
-
-// Private helpers
-
-func Build_half_index(
-	commits CommitHistory,
-	init_lengths *map[string]int,
-) (*HalfIndex, *map[string]int) {
-	half_index := make(HalfIndex)
-	file_blames := make(map[string]BlameSegments)
-	file_commit_hashes := make(map[string]string)
-
-	if init_lengths != nil {
-		for path, length := range *init_lengths {
-			file_blames[path] = BlameSegments{{length, ""}}
-			file_commit_hashes[path] = ""
-		}
-	}
-
-	var ok bool
-	var olineno int		// line numbers, so 1-based, not 0-based
-	var nlineno int
-	var newb BlameSegments
-	var oldb BlameSegments
+func (commit FileCommit) step(oldb BlameSegments) (BlameSegments) {
+	newb := BlameSegments{}
+	olineno := 1
+	nlineno := 1
 
 	oi := 0
 	ocount := 0
+	if len(oldb) > 0 {
+		ocount = oldb[0].LineCount
+	}
 
 	// fmt.Print("===================\n")
-	// fmt.Print("Initial lengths: ", init_lengths, "\n")
+	// fmt.Print("Initial lengths: ", initLengths, "\n")
 	// fmt.Print("Initial blames: ", file_blames, "\n")
 
 	ff := func(linecount int) {
@@ -205,103 +167,59 @@ func Build_half_index(
 		nlineno += linecount
 	}
 
-	for _, commit := range commits {
-		// fmt.Print("COMMIT ", commit.Hash, "\n")
-		// Each unchanged file will keep existing in the next
-		// revision, so this first loop creates a pointer for
-		// every file back to its most recent modification.  The
-		// next loop will overwrite the pointers for files
-		// changed in this revision.
-		for path, commit_hash := range file_commit_hashes {
-			key := fmt.Sprintf("%s:%s", commit.Hash, path)
-			half_index[key] = HalfEntry{commit_hash,
-				BlameSegments{}}
+	// fmt.Print("COMMIT ", commit.Hash, "\n")
+
+	// fmt.Print("A\n")
+	for _, h := range(commit.Hunks) {
+		// fmt.Print("HUNK ", h, "\n")
+		if h.OldLength > 0 {
+			ff(h.OldStart - olineno)
+			skip(h.OldLength)
 		}
-
-		for _, file := range commit.Files {
-			// fmt.Print("PATH ", file.Path, "\n")
-			oldb, ok = file_blames[file.Path]
-			if !ok {
-				oldb = BlameSegments{}
-			}
-
-			previous_commit := file_commit_hashes[file.Path]
-			newb = BlameSegments{}
-
-			olineno = 1
-			nlineno = 1
-
-			oi = 0
+		if h.NewLength > 0 {
+			ff(h.NewStart - nlineno)
+			add(h.NewLength, commit.Hash)
+		}
+	}
+	// fmt.Print("B\n")
+	for oi < len(oldb) {
+		// fmt.Print("Trying to ff", ocount, "\n")
+		if ocount > 0 {
+			ff(ocount)
+		} else {
+			oi += 1
 			ocount = 0
-			if len(oldb) > 0 {
-				ocount = oldb[0].LineCount
-			}
-
-			// fmt.Print("A\n")
-			for _, h := range(file.Hunks) {
-				// fmt.Print("HUNK ", h, "\n")
-				if h.Old_length > 0 {
-					ff(h.Old_start - olineno)
-					skip(h.Old_length)
-				}
-				if h.New_length > 0 {
-					ff(h.New_start - nlineno)
-					add(h.New_length, commit.Hash)
-				}
-			}
-			// fmt.Print("B\n")
-			for oi < len(oldb) {
-				// fmt.Print("Trying to ff", ocount, "\n")
-				if ocount > 0 {
-					ff(ocount)
-				} else {
-					oi += 1
-					ocount = 0
-					if oi < len(oldb) {
-						ocount = oldb[oi].LineCount
-					}
-				}
-			}
-			// fmt.Print("C\n")
-                        key := fmt.Sprintf("%s:%s", commit.Hash, file.Path)
-			half_index[key] = HalfEntry{previous_commit, newb}
-
-			file_blames[file.Path] = newb
-			file_commit_hashes[file.Path] = commit.Hash
-		}
-	}
-
-	file_lengths := make(map[string]int)
-	for path, segments := range file_blames {
-		file_lengths[path] = sum_lines(segments)
-	}
-
-	//fmt.Print("RETURNING: ", half_index, "\nAND: ", file_lengths, "\n")
-	return &half_index, &file_lengths
-}
-
-func reverse_history_in_place(commits CommitHistory) {
-	// Reverse the order of the commits themselves.
-	half := len(commits) / 2
-	last := len(commits) - 1
-	for i := 0; i < half; i++ {
-		commits[i], commits[last-i] =
-			commits[last-i], commits[i]
-	}
-
-	// Reverse the effect of each hunk.
-	for i := range commits {
-		for j := range commits[i].Files {
-			for k := range commits[i].Files[j].Hunks {
-				h := &commits[i].Files[j].Hunks[k]
-				h.Old_start, h.New_start =
-					h.New_start, h.Old_start
-				h.Old_length, h.New_length =
-					h.New_length, h.Old_length
+			if oi < len(oldb) {
+				ocount = oldb[oi].LineCount
 			}
 		}
 	}
+	//fmt.Print("RETURNING: ", half_index, "\nAND: ", fileLengths, "\n")
+	return newb
 }
+
+// func reverse_history_in_place(commits CommitHistory) {
+// 	// Reverse the order of the commits themselves.
+// 	half := len(commits) / 2
+// 	last := len(commits) - 1
+// 	for i := 0; i < half; i++ {
+// 		commits[i], commits[last-i] =
+// 			commits[last-i], commits[i]
+// 	}
+
+// 	// Reverse the effect of each hunk.
+// 	for i := range commits {
+// 		for j := range commits[i].Files {
+// 			for k := range commits[i].Files[j].Hunks {
+// 				h := &commits[i].Files[j].Hunks[k]
+// 				h.OldStart, h.NewStart =
+// 					h.NewStart, h.OldStart
+// 				h.OldLength, h.NewLength =
+// 					h.NewLength, h.OldLength
+// 			}
+// 		}
+// 	}
+// }
 
 func sum_lines(segments BlameSegments) (int) {
 	n := 0
@@ -311,7 +229,7 @@ func sum_lines(segments BlameSegments) (int) {
 	return n
 }
 
-func flatten_segments(segments BlameSegments) (BlameVector) {
+func (segments BlameSegments) flatten() (BlameVector) {
 	v := BlameVector{}
 	for _, segment := range segments {
 		for i := 0; i < segment.LineCount; i++ {
--- blameworthy/indexer_test.go
+++ blameworthy/indexer_test.go
@@ -1,250 +1,85 @@
 package blameworthy
 
 import (
-	"reflect"
-	"strings"
+	"fmt"
 	"testing"
 )
 
-type HalfBlameResult struct {
-	PreviousCommit string
-	Vector BlameVector
-}
-
-type ExpectedHalfBlameResults map[string]HalfBlameResult
-
-func TestHalfIndexing(t *testing.T) {
+func TestIndexing(t *testing.T) {
 	var tests = []struct {
-		input_commits CommitHistory
-		expected_output ExpectedHalfBlameResults
+		inputCommits FileHistory
+		expectedOutput string
 	}{{
-		CommitHistory{},
-		ExpectedHalfBlameResults{},
+		FileHistory{},
+		"[]",
 	}, {
-		CommitHistory{
-			Commit{"a1", []FileHunks{
-				FileHunks{"README", []Hunk{
-					Hunk{0,0,1,3},
-				}},
+		FileHistory{
+			FileCommit{"a1", []Hunk{
+				Hunk{0,0,1,3},
 			}},
 		},
-		ExpectedHalfBlameResults{
-			"a1:README": {"",
-				BlameVector{"a1","a1","a1"}},
-		},
+		"[[{3 a1}]]",
 	}, {
-		CommitHistory{
-			Commit{"a1", []FileHunks{
-				FileHunks{"README", []Hunk{
-					Hunk{0,0,1,3},
-				}},
+		FileHistory{
+			FileCommit{"a1", []Hunk{
+				Hunk{0,0,1,3},
 			}},
-			Commit{"b2", []FileHunks{
-				FileHunks{"README", []Hunk{
-					Hunk{1,0,2,2},
-					Hunk{2,0,5,2},
-				}},
+			FileCommit{"b2", []Hunk{
+				Hunk{1,0,2,2},
+				Hunk{2,0,5,2},
 			}},
-			Commit{"c3", []FileHunks{
-				FileHunks{"README", []Hunk{
-					Hunk{1,1,1,0},
-					Hunk{4,2,3,1},
-				}},
+			FileCommit{"c3", []Hunk{
+				Hunk{1,1,1,0},
+				Hunk{4,2,3,1},
 			}},
 		},
-		ExpectedHalfBlameResults{
-			"a1:README": {"",
-				BlameVector{"a1","a1","a1"}},
-			"b2:README": {"a1",
-				BlameVector{"a1","b2","b2","a1","b2","b2","a1"}},
-			"c3:README": {"b2",
-				BlameVector{"b2","b2","c3","b2","a1"}},
-		},
+		"[[{3 a1}]" +
+			" [{1 a1} {2 b2} {1 a1} {2 b2} {1 a1}]" +
+			" [{2 b2} {1 c3} {1 b2} {1 a1}]]",
 	}, {
-		CommitHistory{
-			Commit{"a1", []FileHunks{
-				FileHunks{"README", []Hunk{
-					Hunk{0,0,1,3},
-				}},
+		FileHistory{
+			FileCommit{"a1", []Hunk{
+				Hunk{0,0,1,3},
 			}},
-			Commit{"b2", []FileHunks{
-				FileHunks{"README", []Hunk{
-					Hunk{1,1,0,0},  // remove 1st line
-					Hunk{2,0,2,1},  // add new line 2
-				}},
+			FileCommit{"b2", []Hunk{
+				Hunk{1,1,0,0},  // remove 1st line
+				Hunk{2,0,2,1},  // add new line 2
 			}},
 		},
-		ExpectedHalfBlameResults{
-			"a1:README": {"", BlameVector{"a1","a1","a1"}},
-			"b2:README": {"a1", BlameVector{"a1","b2","a1"}},
-		},
+		"[[{3 a1}] [{1 a1} {1 b2} {1 a1}]]",
 	}, {
-		CommitHistory{
-			Commit{"a1", []FileHunks{
-				FileHunks{"README", []Hunk{
-					Hunk{0,0,1,3},
-				}},
+		FileHistory{
+			FileCommit{"a1", []Hunk{
+				Hunk{0,0,1,3},
 			}},
-			Commit{"b2", []FileHunks{
-				FileHunks{"README", []Hunk{
-					Hunk{1,3,0,0},
-				}},
+			FileCommit{"b2", []Hunk{
+				Hunk{1,3,0,0},
 			}},
 		},
-		ExpectedHalfBlameResults{
-			"a1:README": {"", BlameVector{"a1","a1","a1"}},
-			"b2:README": {"a1", BlameVector{}},
-		},
+		"[[{3 a1}] []]",
 	}, {
-		CommitHistory{
-			Commit{"a1", []FileHunks{
-				FileHunks{"README", []Hunk{
-					Hunk{0,0,1,3},
-				}},
+		FileHistory{
+			FileCommit{"a1", []Hunk{
+				Hunk{0,0,1,3},
 			}},
-			Commit{"b2", []FileHunks{
-				FileHunks{"hello.c", []Hunk{
-					Hunk{0,0,1,2},
-				}},
-			}},
-			Commit{"c3", []FileHunks{
-				FileHunks{"README", []Hunk{
-					Hunk{0,0,4,1},
-				}},
+			FileCommit{"b2", []Hunk{
+				Hunk{0,0,4,1},
 			}},
 		},
-		ExpectedHalfBlameResults{
-			"a1:README": {"", BlameVector{"a1","a1","a1"}},
-			"b2:README": {"a1", BlameVector{}},
-			"b2:hello.c": {"", BlameVector{"b2", "b2"}},
-			"c3:README": {"a1", BlameVector{"a1","a1","a1","c3"}},
-			"c3:hello.c": {"b2", BlameVector{}},
-		},
+		"[[{3 a1}] [{3 a1} {1 b2}]]",
 	}}
-	for test_number, c := range tests {
-		index, _ := Build_half_index(c.input_commits, nil)
-		for key, desired := range c.expected_output {
-			actual := (*index)[key]
-			actual_vector := flatten_segments(actual.segments)
-			t1 := desired.PreviousCommit == actual.PreviousCommit
-			t2 := reflect.DeepEqual(desired.Vector, actual_vector)
-			if (!(t1 && t2)) {
-				t.Error("Test", test_number, "key", key,
-					"failed\n  Wanted", desired.Vector,
-					"\n  Got   ", actual_vector,
-					"\n  From  ", actual.segments)
-			}
+	for test_number, test := range tests {
+		segments := BlameSegments{}
+		out := []BlameSegments{}
+		for _, commit := range test.inputCommits {
+			segments = commit.step(segments)
+			out = append(out, segments)
 		}
-	}
-}
-
-type ExpectedBlameResults map[string]BlameResult
-
-func TestFullIndexing(t *testing.T) {
-	var tests = []struct {
-		input_commits CommitHistory
-		expected_output ExpectedBlameResults
-	}{{
-		CommitHistory{},
-		ExpectedBlameResults{},
-	}, {
-		CommitHistory{
-			Commit{"a1", []FileHunks{
-				FileHunks{"README", []Hunk{
-					Hunk{0,0,1,2},
-				}},
-			}},
-			Commit{"b2", []FileHunks{
-				FileHunks{"README", []Hunk{
-					Hunk{1,0,2,1},
-				}},
-			}},
-			Commit{"c3", []FileHunks{
-				FileHunks{"README", []Hunk{
-					Hunk{3,1,3,0},
-				}},
-			}},
-		},
-		ExpectedBlameResults{
-			"a1:README": {"", "b2",
-				BlameVector{"a1","a1"},
-				BlameVector{"","c3"}},
-			"b2:README": {"a1", "c3",
-				BlameVector{"a1","b2","a1"},
-				BlameVector{"","","c3"}},
-			"c3:README": {"b2", "",
-				BlameVector{"a1","b2"},
-				BlameVector{"",""}},
-		},
-	}, {
-		CommitHistory{
-			Commit{"a1", []FileHunks{
-				FileHunks{"README", []Hunk{
-					Hunk{0,0,1,3},
-				}},
-			}},
-			Commit{"b2", []FileHunks{
-				FileHunks{"hello.c", []Hunk{
-					Hunk{0,0,1,2},
-				}},
-			}},
-			Commit{"c3", []FileHunks{
-				FileHunks{"README", []Hunk{
-					Hunk{2,1,1,0},
-					Hunk{3,0,3,1},
-				}},
-			}},
-		},
-		ExpectedBlameResults{
-			"a1:README": {"", "c3",
-				BlameVector{"a1","a1","a1"},
-				BlameVector{"","c3",""}},
-			// "a1:hello.c": {"", "b2"},  TODO: pre-existence pointers?
-			"b2:README": {"a1", "c3",
-				BlameVector{}, BlameVector{}},
-			"b2:hello.c": {"", "",
-				BlameVector{"b2","b2"},
-				BlameVector{"",""}},
-			"c3:README": {"a1", "",
-				BlameVector{"a1","a1","c3"},
-				BlameVector{"","",""}},
-			"c3:hello.c": {"b2", "",
-				BlameVector{}, BlameVector{}},
-		},
-	}, {
-		CommitHistory{
-			Commit{"a1", []FileHunks{
-				FileHunks{"tools/gen_build.go", []Hunk{
-					Hunk{0,0,1,3},
-				}},
-			}},
-			Commit{"b2", []FileHunks{
-				FileHunks{"tools/gen_build.go", []Hunk{
-					Hunk{1,3,0,0}, // file deletion
-				}},
-			}},
-		},
-		ExpectedBlameResults{
-			"a1:tools/gen_build.go": {"", "b2",
-				BlameVector{"a1","a1","a1"},
-				BlameVector{"b2","b2","b2"}},
-			"b2:tools/gen_build.go": {"a1", "",
-				BlameVector{},
-				BlameVector{}},
-		},
-	}}
-	for test_number, c := range tests {
-		index := Build_index(c.input_commits)
-		for key, desired_result := range c.expected_output {
-			i := strings.Index(key, ":")
-			commit_hash := key[:i]
-			path := key[i+1:]
-			actual_result, _ := index.GetNode(commit_hash, path)
-			if !reflect.DeepEqual(&desired_result, actual_result) {
-				t.Error("Test", test_number, "key", key,
-					"failed\n  Wanted", desired_result,
-					"\n  Got   ", actual_result)
-			}
+		if (fmt.Sprint(out) != test.expectedOutput) {
+			t.Error("Test", test_number + 1, "failed",
+				"\n  Wanted", test.expectedOutput,
+				"\n  Got   ", fmt.Sprint(out),
+				"\n  From  ", test.inputCommits)
 		}
 	}
 }
