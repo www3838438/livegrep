3e5ffbc1a474ad3e96034895152e31508bd34bae
Brandon Rhodes <brhodes@dropbox.com>
2018-01-17 14:28:01 -0500

    Turn line numbers into named anchors

--- web/htdocs/assets/raw-js/blame.js
+++ web/htdocs/assets/raw-js/blame.js
@@ -1,15 +1,17 @@
 (function f() {
     var body = $('body');
     body.on('mouseenter', '#hashes > a', function(e) {
-        var href = $(e.target).attr('href');
+        var href = $(e.target).attr('href') || "";
         var i = href.indexOf('#');
+        if (i == -1) return;
         var commitHash = href.substring(0, i);
         var cls = 'highlight ' + href.substr(i + 1, 1);
         $('#hashes a[href^="' + commitHash + '"]').addClass(cls);
     });
     body.on('mouseleave', '#hashes > a', function(e) {
-        var href = $(e.target).attr('href');
+        var href = $(e.target).attr('href') || "";
         var i = href.indexOf('#');
+        if (i == -1) return;
         var commitHash = href.substring(0, i);
         var cls = 'highlight ' + href.substr(i + 1, 1);
         $('#hashes a[href^="' + commitHash + '"]').removeClass(cls);
